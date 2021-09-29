15ff814e1488b92bc9f2c8f54397c3425b83bc8c
Andy <ay83103@gmail.com>
2021-09-29 23:01:34 +0800

    完成

--- InvestingCrawler.py
+++ InvestingCrawler.py
@@ -1,9 +1,51 @@
 import HtmlRequest
 
-headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
+def gethtml(starttime,endtime,url):
+    data={"curr_id": 6408,
+    "smlID": 1159963,
+    "header": "AAPL历史数据",
+    "st_date": starttime,
+    "end_date": endtime,
+    "interval_sec": "Daily",
+    "sort_col": "date",
+    "sort_ord": "DESC",
+    "action": "historical_data"}
 
-response=HtmlRequest.htmlparse('get',f"https://cn.investing.com/equities/apple-computer-inc-historical-data",data="",headers=headers)
+    headers={
+    "content-type":"application/x-www-form-urlencoded",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "x-requested-with":"XMLHttpRequest"
+    }
 
-table=response.find(id="curr_table")
+    response=HtmlRequest.htmlparse('post',url,data,headers)
+    return response
 
-print(table)
+def getresult(htmldata):
+    result=[]
+
+    table=htmldata.find(id="curr_table")
+    thead=table.find('thead')
+
+    #結果的key
+    date=thead.find(attrs={'data-col-name':'date'}).text
+    price=thead.find(attrs={'data-col-name':'price'}).text
+    open=thead.find(attrs={'data-col-name':'open'}).text
+    high=thead.find(attrs={'data-col-name':'high'}).text
+    low=thead.find(attrs={'data-col-name':'low'}).text
+    vol=thead.find(attrs={'data-col-name':'vol'}).text
+    change=thead.find(attrs={'data-col-name':'change'}).text
+    
+    tr=table.find('tbody').findAll('tr')
+
+    keylist=[date,price,open,high,low,vol,change]
+    for item in tr:
+        elements=item.findAll('td')
+        data={}
+        counter=0
+
+        for element in elements:
+            data[keylist[counter]]=element.text
+            counter+=1
+        result.append(data)  
+
+    return result
